d437f8d208531f33360ddba64a81945c5e106adb
dappnodedev <diego@dappnode.io>
2024-07-25 12:22:34 +0200

    Use latest staker scripts version

--- reth/entrypoint.sh
+++ reth/entrypoint.sh
@@ -5,33 +5,29 @@ SUPPORTED_NETWORKS="holesky mainnet"
 # shellcheck disable=SC1091
 . /etc/profile
 
-run_client() {
-  echo "[INFO - entrypoint] Running client"
+JWT_PATH=$(get_jwt_path "${NETWORK}" "${SUPPORTED_NETWORKS}" "${SECURITY_PATH}")
 
-  echo "${JWT_PATH}"
+post_jwt_to_dappmanager "${JWT_PATH}"
 
-  # shellcheck disable=SC2086
-  exec reth \
-    node \
-    --full \
-    --chain "${NETWORK}" \
-    --metrics 0.0.0.0:6060 \
-    --datadir "${DATA_DIR}" \
-    --addr 0.0.0.0 \
-    --port "${P2P_PORT}" \
-    --http \
-    --http.addr 0.0.0.0 \
-    --http.port 8545 \
-    --http.corsdomain "*" \
-    --ws \
-    --ws.addr 0.0.0.0 \
-    --ws.port 8546 \
-    --ws.origins "*" \
-    --authrpc.addr 0.0.0.0 \
-    --authrpc.port 8551 \
-    --authrpc.jwtsecret "${JWT_PATH}" ${EXTRA_OPTS}
-}
+echo "[INFO - entrypoint] Running Reth client for network: ${NETWORK}"
 
-set_execution_config_by_network "${NETWORK}" "${SUPPORTED_NETWORKS}"
-post_jwt_to_dappmanager
-run_client
+# shellcheck disable=SC2086
+exec reth \
+  node \
+  --full \
+  --chain "${NETWORK}" \
+  --metrics 0.0.0.0:6060 \
+  --datadir "${DATA_DIR}" \
+  --addr 0.0.0.0 \
+  --port "${P2P_PORT}" \
+  --http \
+  --http.addr 0.0.0.0 \
+  --http.port 8545 \
+  --http.corsdomain "*" \
+  --ws \
+  --ws.addr 0.0.0.0 \
+  --ws.port 8546 \
+  --ws.origins "*" \
+  --authrpc.addr 0.0.0.0 \
+  --authrpc.port 8551 \
+  --authrpc.jwtsecret "${JWT_PATH}" ${EXTRA_OPTS}
